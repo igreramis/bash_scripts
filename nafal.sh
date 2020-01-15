d89f4d5581f16cb8d9d81521ec59a3dd60c7091c
igreramis <igreramis@gmail.com>
2020-01-15 22:24:43 +1100

    working condition for script achieved

--- nafal.sh
+++ nafal.sh
@@ -1,30 +1,44 @@
 #!/bin/bash
-
+file="nafal_records.txt"
 helpFunction()
 {
    echo ""
-   echo "Usage: $0 -n num_nawafil_today"
+   echo "Usage: $0 [-n num_nawafil_today] [-t display total num of nafal recorded]"
    echo -e "\t-n Numbers Of Nawafils Today"
+   echo -e "\t-t display total num of nafal recorded"
    exit 1 # Exit script after printing help
 }
 
-while getopts "n:" opt
+update_records()
+{
+	base_10="10#"
+	base_10_num_nafal_today=$base_10$num_nafal_today
+	echo "`date`-$base_10_num_nafal_today" >> $file
+}
+
+analyze_records()
+{ 	
+	total_nafal_completed=0
+	while IFS= read -r line
+	do
+		nafal=`echo $line|awk -F- '{print $2}'`
+		total_nafal_completed=$((total_nafal_completed + nafal))
+	done < "$file"
+	echo "total_nafal_completed: $total_nafal_completed"
+}
+
+while getopts "n:t" opt
 do
    case "$opt" in
-      n ) parameterA="$OPTARG" ;;
-      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+      n ) num_nafal_today="$OPTARG"
+		  if [ -z "$num_nafal_today" ]
+		  then
+			echo "Some or all of the parameters are empty";
+			helpFunction
+		  fi
+		  update_records ;;
+	  t ) analyze_records ;;
+      ? ) helpFunction ;;
 	  : ) echo "Entering the fucking argument for the option dumbfuck!!!"
    esac
 done
-
-# Print helpFunction in case parameters are empty
-if [ -z "$parameterA" ] || [ -z "$parameterB" ] || [ -z "$parameterC" ]
-then
-   echo "Some or all of the parameters are empty";
-   helpFunction
-fi
-
-# Begin script in case all parameters are correct
-echo "$parameterA"
-echo "$parameterB"
-echo "$parameterC"	
